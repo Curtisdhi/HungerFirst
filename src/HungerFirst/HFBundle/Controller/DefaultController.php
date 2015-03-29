af5f570ffa4584f1466d502fbcb5d637cabb8475
Pickle15 <dill16pickle1993@gmail.com>
2015-03-29 02:09:38 -0400

    Added bootstrap

--- src/HungerFirst/HFBundle/Controller/DefaultController.php
+++ src/HungerFirst/HFBundle/Controller/DefaultController.php
@@ -6,8 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('HFBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('HFBundle:Default:index.html.twig');
     }
 }
