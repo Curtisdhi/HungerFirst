e40b4dfaeddd4cd6304c1b07f610bbf8e53d5426
Pickle15 <dill16pickle1993@gmail.com>
2015-03-29 21:38:17 -0400

    Added a edit customer controller

--- src/HungerFirst/HFBundle/Controller/CustomerController.php
+++ src/HungerFirst/HFBundle/Controller/CustomerController.php
@@ -15,8 +15,8 @@ class CustomerController extends Controller
     }
     
     public function createAction(Request $request) {
-        //$securityContext = $this->container->get('security.context');
-        //if($securityContext->isGranted('IS_AUTHENTICATED_FULLY')){
+        $securityContext = $this->container->get('security.context');
+        if($securityContext->isGranted('IS_AUTHENTICATED_FULLY')){
             $customer = new Customer();
             $form = $this->createForm(new CustomerType(), $customer);
             $form->handleRequest($request);
@@ -28,15 +28,46 @@ class CustomerController extends Controller
                
                $em->flush();
                
-               //$this->get('session')->getFlashBag()->set('success', 'This is your new gift page!');
+               $this->get('session')->getFlashBag()->set('success', 'Successfully created new customer.');
                
                return $this->redirectToRoute('hf_homepage');
             }
             
             return $this->render('HFBundle:Customer:create.html.twig', array('form' => $form->createView()));
-        //}
-        //else {
-        //    return $this->redirectToRoute('fos_user_security_login');
-        //}
+        }
+        else {
+            return $this->redirectToRoute('fos_user_security_login');
+        }
+    }
+    
+    public function editAction(Request $request, $id) {
+        $securityContext = $this->container->get('security.context');
+        if($securityContext->isGranted('IS_AUTHENTICATED_FULLY')){
+            $em = $this->getDoctrine()->getManager();
+            $customer = $em->find("HFBundle:Customer", $id);
+            if (!$customer) {
+                throw $this->createNotFoundException('This customer doesn\'t exist');
+            }
+
+            $form = $this->createForm(new CustomerType(), $customer);
+            $form->handleRequest($request);
+            if ($form->isValid()) {
+               $em = $this->getDoctrine()->getManager();
+               $customer = $form->getData();
+
+               $em->persist($customer);
+               
+               $em->flush();
+               
+               $this->get('session')->getFlashBag()->set('success', 'Successfully edited customer');
+               
+               return $this->redirectToRoute('hf_homepage');
+            }
+            
+            return $this->render('HFBundle:Customer:create.html.twig', array('form' => $form->createView()));
+        }
+        else {
+            return $this->redirectToRoute('fos_user_security_login');
+        }
     }
 }
